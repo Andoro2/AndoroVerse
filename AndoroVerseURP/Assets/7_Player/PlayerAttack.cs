7840dca0ea9decd0e29af8a333d36c7cb4ed456e
Andreu Llàcer <llacer.andreu@gmail.com>
2023-11-02 19:00:00 +0100

    Carpetas Assets\6_GUI + Assets\7_Player

---
+++ AndoroVerseURP/Assets/7_Player/PlayerAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float HitDmg;
+    public GameObject m_HitVFX;
+    private AudioSource m_AS;
+    public AudioClip m_AttackAudio;
+
+    private void Start()
+    {
+        m_AS = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Instantiate(m_HitVFX, transform.position, Quaternion.identity);
+
+            Vector3 AttackVector = other.transform.position - transform.parent.parent.transform.position;
+
+            Vector3 KnockBackDirection = new Vector3(AttackVector.x, 0f, AttackVector.z);
+
+            other.GetComponent<EnemyLifeController>().TakeDamage(MainScript.PlayerMeleDamage, KnockBackDirection, MainScript.PlayerKBForce);
+
+            if(m_AS != null)
+            {
+                m_AS.clip = m_AttackAudio;
+
+                m_AS.Play();
+            }
+        }
+    }
+}
